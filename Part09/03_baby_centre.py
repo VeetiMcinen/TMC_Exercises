bf083cf915d6c1142ee390ff47b6522307d9a681
VeetiMcinen <veeti.makinen141@gmail.com>
2024-10-01 13:34:31 +0300

    Added part 9 exercise 3

---
+++ Part09/03_baby_centre.py
@@ -0,0 +1,37 @@
+baby_centre = BabyCentre()
+
+eric = Person("Eric", 1, 110, 7)
+peter = Person("Peter", 33, 176, 85)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre = BabyCentre()
+
+eric = Person("Eric", 1, 110, 7)
+peter = Person("Peter", 33, 176, 85)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
