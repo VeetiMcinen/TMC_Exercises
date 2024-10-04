27ed2c5c293468f2663d699407cb6701419b472e
VeetiMcinen <veeti.makinen141@gmail.com>
2024-10-04 13:55:57 +0300

    changed part 10 exec 2

--- Part10/2_museum.py
+++ Part10/2_museum.py
@@ -3,31 +3,25 @@ class ComputerGame:
         self.name = name
         self.publisher = publisher
         self.year = year
-
+ 
 class GameWarehouse:
     def __init__(self):
         self.__games = []
-
+ 
     def add_game(self, game: ComputerGame):
         self.__games.append(game)
-
+ 
     def list_games(self):
         return self.__games
-
+ 
 class GameMuseum(GameWarehouse):
     def __init__(self):
         super().__init__()
+ 
     def list_games(self):
-        newgames = []
-        for i in self._GameWarehouse__games:
-            if i.year <= 1990:
-                newgames.append(i)
-        return newgames
-
-
-museum = GameMuseum()
-museum.add_game(ComputerGame("Pacman", "Namco", 1980))
-museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
-museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
-for game in museum.list_games():
-    print(game.name)
+        gamelist = []
+        for game in super().list_games():
+            if game.year < 1990:
+                gamelist.append(game)
+ 
+        return gamelist
